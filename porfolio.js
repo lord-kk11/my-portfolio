7353e366bd88a2655e0f9a568d6ed7ad20dfe278
Lodrick <letemlodrick@gmail.com>
2025-08-31 22:19:21 +0100

    added new UI feature

--- porfolio.js
+++ porfolio.js
@@ -25,40 +25,103 @@ blocks.forEach((block) => {
   });
 });
 
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+gsap.utils.toArray(".tech-stack").forEach((el, i) => {
+
+  let t1 = gsap.timeline({
+    scrollTrigger: {
+      trigger:el,
+      start: "top 80%",
+      toggleActions: "play none none reverse"
+    }
+  });
+
+  //animate icon
+  t1.from(el,{
+    scale:0.2,
+    opacity:0,
+    duration: 0.8,
+    ease: "back.out(1.7)"
+  });
+
+    //animate border
+    t1.from(el,{
+      borderColor: "#070707ff",
+      duration: 0.8
+    }, "<")
+
+    t1.to(el.querySelector(".tech-name"),{
+      opacity:1,
+      duration:0.5
+    }, ">");
+
+
+});
+
+
+
 moonGrid.addEventListener("mouseenter", () => {
-  moonMessage.insertAdjacentText("beforeend", "click me");
+  moonMessage.innerHTML = ""
+  moonMessage.insertAdjacentText("beforeend", "click");
 });
 
 moonGrid.addEventListener("mouseleave", () => {
-  moonMessage.textContent = moonMessage.textContent.replace("click me", "");
+  moonMessage.textContent = moonMessage.textContent.replace("click", addtextonmouseleave());
+  
+  
 });
+
+
+function addtextonmouseleave() {
+  if(!moon ){
+    return "dark mode"
+  }else{
+    return "Light mode"
+  }
+}
+
+
 
 let savedtheme = localStorage.getItem("theme");
 const html = document.documentElement;
+
+  
+const t2 = gsap.timeline();
+
 
 if (savedtheme == "darkmode") {
   html.classList.add("darkmode");
   html.classList.remove("lightmode");
   mainmoon.classList.add("fa-sun");
   mainmoon.classList.remove("fa-moon");
-  
+      moonMessage.innerHTML = "dark mode";
+   t2.to("#moon", {duration:4, x:"65vw", ease: "power2.inOut"})
 } else {
   html.classList.add("lightmode");
   html.classList.remove("darkmode");
   mainmoon.classList.add("fa-moon");
   mainmoon.classList.remove("fa-sun");
-
+  moonMessage.innerHTML = "light-mode";
+   t2.to("#moon", {duration:4, x:"0vw", ease: "power2.inOut"})
 }
 
-let moon = savedtheme === "darkmode";
+
+
+
+
+
+
+let moon = savedtheme === "dark mode";
 
 const t1 = gsap.timeline();
 
 t1.to(".n-a", { duration: 1, x: 20 })
   .to(".name", { duration: 1, x: 20 })
   .to(".n-s", { duration: 1, x: 20 });
-  
-const t2 = gsap.timeline();
+
 
 
 mainmoon.addEventListener("click", () => {
@@ -68,14 +131,16 @@ mainmoon.addEventListener("click", () => {
     localStorage.setItem("theme", "darkmode");
     mainmoon.classList.remove("fa-moon");
     mainmoon.classList.add("fa-sun");
-     t2.to("#moon", {duration:2, x:1000})
+    moonMessage.innerHTML = "dark-mode";
+     t2.to("#moon", {duration:4, x:700, ease: "power2.inOut"})
   } else {
     html.classList.add("lightmode");
     html.classList.remove("darkmode");
     localStorage.setItem("theme", "lightmode");
     mainmoon.classList.remove("fa-sun");
     mainmoon.classList.add("fa-moon");
-     t2.to("#moon", {duration:2, x:0})
+    moonMessage.innerHTML = "light-mode";
+     t2.to("#moon", {duration:4, x:0, ease: "power2.inOut"})
   }
   moon = !moon;
   t1.restart();
